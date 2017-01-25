5a7441829820bee7da37cceef6322db2c9f7605e
sheph2mj <sheph2mj@cmich.edu>
2017-01-25 10:25:39 -0500

    new user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
-  def new
-  end
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def update
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'users/new'
   get '/foo/', to: 'item#index'
+  resources :users
 end
