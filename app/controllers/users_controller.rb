566b20a621670771081ab8c885f5ca8ea6e1ac4a
marcusshepp <sheph2mj@cmich.edu>
2017-01-25 11:37:26 -0500

    users can now create profiles but w/o validation and authenticated and rmaining logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,27 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to @user
+		else
+			render "new"
+		end
+	end
+
     def update
     end
+
+	private
+
+	def user_params
+		params.require(
+			:user).permit(
+			:username, 
+			:email, 
+			:password, 
+			:password_confirmation)
+	end
+
 end
