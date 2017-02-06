e03d2d86761002eaaabdf0e28d320470f0669f3f
Dreyton Scott <drekscott@icloud.com>
2017-02-06 12:22:02 -0500

    Create a budget logic

--- db/migrate/20170124183043_create_budgets.rb
+++ db/migrate/20170124183043_create_budgets.rb
@@ -2,6 +2,7 @@ class CreateBudgets < ActiveRecord::Migration[5.0]
   def change
     create_table :budgets do |t|
       t.decimal :amount
+      t.decimal :current_amount
       t.integer :user_id, :unique => true
       t.timestamps
     end
