fd501062e75ff277fcd18b8a0aa4dff7f0ef911c
Dreyton Scott <drekscott@icloud.com>
2017-02-06 22:56:12 -0500

    Should be able to see list of categories and edit them

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,6 +11,27 @@ class CategoriesController < ApplicationController
         end
     end
 
+    def show
+      @category = Category.find(params[:id])
+    end
+
+    def edit
+      @category = Category.find(params[:id])
+    end
+
+    def update
+      @category = Category.find(params[:id])
+      if @category.update(category_params)
+        redirect_to @category
+      else
+        render 'edit'
+      end
+    end
+
+    def index
+      @categories = Category.all
+    end
+
     private
     def category_params
         params.require(
