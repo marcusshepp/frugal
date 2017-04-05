b81a7afb0972a7d8bfa4a6c6b3e70962012e9f0d
marcusshepp <sheph2mj@cmich.edu>
2017-04-05 11:20:57 -0400

    css made some buttons disappear

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
 		if @user.save
             log_in @user
             flash[:success] = "Welcome!!!!"
-			redirect_to @user
+			redirect_to items_url
 		else
 			render 'new'
 		end
