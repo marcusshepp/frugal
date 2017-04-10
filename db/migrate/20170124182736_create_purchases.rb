214e5546be45942f3beab1facd2d25cbd89940e2
marcusshepp <sheph2mj@cmich.edu>
2017-04-10 11:24:16 -0400

    user_id on purchases

--- db/migrate/20170124182736_create_purchases.rb
+++ db/migrate/20170124182736_create_purchases.rb
@@ -2,6 +2,7 @@ class CreatePurchases < ActiveRecord::Migration[5.0]
   def change
     create_table :purchases do |t|
       t.integer :item_id
+      t.integer :user_id
       t.decimal :amount
       t.timestamps
     end
